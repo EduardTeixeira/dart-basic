ae56819700e56813e2f98b38b99aa20395c1826a
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-10-29 17:24:30 -0300

    'string'

---
+++ string.dart
@@ -0,0 +1,30 @@
+void main() {
+  // tipo dinamico
+  var a;
+  a = 'Dart';
+
+  // tipo var com valor incial
+  var b = 'Eduard Teixeira';
+
+  // tipo explicito
+  String c;
+  c = 'Eduard';
+
+  // tipo explicito com valor inicial
+  String d = 'Flutter';
+
+  // boa pratica de declaracao de variavel
+  var aa = 'Eduard';
+
+  // má pratica de declaracao de variavel
+  var bb;
+  b = 'Eduard';
+
+  // boa pratica de declaracao de variavel
+  String cc;
+  cc = 'Eduard';
+
+  print('Concatenar Strings');
+  var result = a + ' ' + b;
+  print(result);
+}
