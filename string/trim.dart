a5b67d1e5dd8b7ed74bbbea6feb9e25b217d57fe
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-10-30 16:24:30 -0300

    trim

---
+++ string/trim.dart
@@ -0,0 +1,12 @@
+void main() {
+  final text = ' Eduard Teixeira ';
+
+  print(text);
+  print(text.length);
+
+  print(text.trim());
+  print(text.trim().length);
+
+  print(text.trimLeft());
+  print(text.trimLeft().length);
+}
