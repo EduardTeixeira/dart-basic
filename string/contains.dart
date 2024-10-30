2e1e46940b44464e7c678fb3aaa4be0a40914bec
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-10-30 16:03:54 -0300

    contains

---
+++ string/contains.dart
@@ -0,0 +1,6 @@
+void main() {
+  final text = 'Lorem ipsum dolor sit amet, consectetur';
+  print(text.contains('ipsum'));
+  print(text.contains('a'));
+  print(text.toLowerCase().contains('a'));
+}
