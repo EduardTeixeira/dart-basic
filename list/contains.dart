7c636bae97d87772cc75f91df0d1000a3c0138fd
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-10-31 15:57:32 -0300

    list - contains()

---
+++ list/contains.dart
@@ -0,0 +1,10 @@
+void main() {
+  var listA = ['a', 'b', 'c', 'd', 'e'];
+  print(listA);
+
+  var exist = listA.contains('c');
+  print(exist);
+
+  exist = listA.contains('h');
+  print(exist);
+}
