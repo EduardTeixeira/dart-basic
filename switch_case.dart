d5d6aa8c8ee57e03ec27ac5e8ba4f834896f0fbc
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-11-13 17:13:23 -0300

    switch case

---
+++ switch_case.dart
@@ -0,0 +1,39 @@
+void main() {
+  var month = 5;
+
+  switch (month) {
+    case 1:
+      print("January has 31 days.");
+      break;
+    case 2:
+      print("February has 28 days.");
+      break;
+    case 3:
+      print("March has 31 days.");
+      break;
+    case 4:
+      print("April has 30 days.");
+      break;
+    case 5:
+      print("May has 31 days.");
+      break;
+    case 6:
+      print("June has 30 days.");
+      break;
+    case 7:
+      print("July has 31 days.");
+      break;
+    case 8:
+      print("August has 31 days.");
+      break;
+    case 9:
+      print("September has 30 days.");
+      break;
+    case 10:
+      print("October has 31 days.");
+      break;
+    default:
+      print("Month not registred.");
+      break;
+  }
+}
