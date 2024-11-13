a0af677cc54d71a199bade7df8f067430da2eade
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-11-13 17:02:53 -0300

    logica or

---
+++ logica_or.dart
@@ -0,0 +1,28 @@
+void main() {
+  var a = 1;
+  var b = 2;
+
+  if ((a > 0) || (b < 3)) {
+    print('VERDADEIRO');
+  } else {
+    print('FALSO');
+  }
+
+  if ((a > 1) || (b < 3)) {
+    print('VERDADEIRO');
+  } else {
+    print('FALSO');
+  }
+
+  if ((a >= 1) || (b < 3)) {
+    print('VERDADEIRO');
+  } else {
+    print('FALSO');
+  }
+
+  if ((a != 1) || (b != 2)) {
+    print('VERDADEIRO');
+  } else {
+    print('FALSO');
+  }
+}
